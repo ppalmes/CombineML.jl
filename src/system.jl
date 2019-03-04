2d66dd821a7ce7edd8ab3f751ea71dbb06181c4e
Paulito Palmes <ppalmes@gmail.com>
2019-03-04 11:07:17 +0800

    rcall binary install in install.packages

--- src/system.jl
+++ src/system.jl
@@ -47,7 +47,6 @@ function check_r_dep()
 end
 
 # Check system for python dependencies.
-#LIB_SKL_AVAILABLE = check_py_dep("scikit-learn")
-LIB_SKL_AVAILABLE = false
+LIB_SKL_AVAILABLE = check_py_dep("scikit-learn")
 LIB_CRT_AVAILABLE = check_r_dep()
 end # module
